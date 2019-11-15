18e00ce2766617fbcd6c938b5350a05ef69bc416
Ólafur Eysteinn <olafurey@gmail.com>
2019-11-15 14:46:41 +0000

    Add files via upload

---
+++ lokaverkefni/insertInto/insertAlignments.sql
@@ -0,0 +1,8 @@
+INSERT INTO Alignments
+VALUES
+    (NULL,"Hero"),
+    (NULL,"Neutral"),
+    (NULL,"Villain");
+
+DELETE Alignments;
+SELECT * FROM Alignments;
--- lokaverkefni/insertInto/insertCharacters.sql
+++ lokaverkefni/insertInto/insertCharacters.sql
@@ -1,9 +1,16 @@
 INSERT INTO Characters
 VALUES
-    (NULL,"Anthony","Edward","Stark","1970-05-29","Manhattan, New York City","Iron Man",1,1,1),
-    (NULL,"",,,,,,,,),
-    (NULL,,,,,,,,,),
-    (NULL,,,,,,,,,);
+    (NULL,"Anthony","Edward","Stark","1970-05-29","USA","Iron Man",1,1,1),
+    (NULL,"James","Rupert","Rhodes","1968-10-06","USA","War Machine",1,1,1),
+    (NULL,"Obadiah",NULL,"Stane",NULL,"USA","Iron Monger",3,1,1),
+    (NULL,"Robert","Bruce","Banner","1969-12-18","USA","Hulk",1,1,1),
+    (NULL,"Emil",NULL,"Blonsky","1971-03-01","Russia","Abomination",3,1,1),
+    (NULL,"Virginia",NULL,"Potts",NULL,"USA",NULL,1,1,2),
+    (NULL,"Natalia","Alianovna","Romanoff","1984-01-01","Russia","Black Widow",1,1,2),
+    (NULL,"Nicholas","Joseph","Fury","1950-07-04","USA","Nick Fury",1,1,1),
+    (NULL,"Ivan","Antonovich","Vanko","1968-02-15","Russia","Whiplash",3,1,1)
+    (NULL,"Thor",NULL,"Odinson","964 A.D.","Asgard","Thor",1,2,1);
+    
 
 SELECT * FROM Characters;
 DELETE FROM Characters;
