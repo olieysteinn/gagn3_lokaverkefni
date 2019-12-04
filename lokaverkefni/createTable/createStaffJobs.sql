f6b07c8dc38f4d0c75e9daf1aba58402c4c8fc38
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:24:47 +0000

    Delete createStaffJobs.sql

--- lokaverkefni/createTable/createStaffJobs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE StaffJobs(
-    id INTEGER PRIMARY KEY,
-    staff_id INT NOT NULL,
-    job_id INT NOT NULL,
-    FOREIGN KEY(staff_id) REFERENCES Staff(id),
-    FOREIGN KEY(job_id) REFERENCES Jobs(id)
-);
-
-DROP TABLE StaffJobs;
