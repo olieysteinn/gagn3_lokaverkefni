9b39d3ed08d36443091d1339b23e62300600ca04
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:22:21 +0000

    Delete createCharacterItems.sql

--- lokaverkefni/createTable/createCharacterItems.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CharacterItems(
-    id INTEGER PRIMARY KEY,
-    character_id INT NOT NULL,
-    item_id INT NOT NULL,
-    FOREIGN KEY(character_id) REFERENCES Characters(id),
-    FOREIGN KEY(item_id) REFERENCES Items(id)
-);
-
-DROP TABLE CharacterItems;
