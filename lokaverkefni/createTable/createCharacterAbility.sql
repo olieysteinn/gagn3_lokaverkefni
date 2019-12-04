b551d7c5000db4fa889ade33d15ab23e7036ff9e
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:21:50 +0000

    Delete createCharacterAbility.sql

--- lokaverkefni/createTable/createCharacterAbility.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CharacterAbilities (
-    id INTEGER PRIMARY KEY,
-    character_id INT NOT NULL,
-    ability_id INT NOT NULL,
-    FOREIGN KEY(character_id) REFERENCES Characters(id),
-    FOREIGN KEY(ability_id) REFERENCES Abilities(id)
-);
-
-DROP TABLE CharacterAbilities;
