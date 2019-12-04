a7e1d1879e72e98a800eb98d0221d9f59ead1b5b
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:26:00 +0000

    Delete insertJobs.sql

--- lokaverkefni/insertInto/insertJobs.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Jobs
-VALUES
-    (NULL,"Actor","Actors portray characters in movies, television and live performances."),
-    (NULL,"Director","A film director is a person who directs the making of a film. A film director controls a film's artistic and dramatic aspects and visualizes the screenplay (or script) while guiding the technical crew and actors in the fulfilment of that vision."),
-    (NULL,"Screenwriter","A screenwriter is a writer who practices the craft of screenwriting, writing screenplays on which mass media, such as films, television programs and video games, are based."),
-    (NULL,"Producer","A film producer is a person who oversees film production.[1] Either employed by a production company or working independently, producers plan and coordinate various aspects of film production, such as selecting the script; coordinating writing, directing, and editing; and arranging financing.");
-
-SELECT * FROM Jobs;
-DELETE FROM Jobs;
