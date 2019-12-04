15188bf1ab82ba95da9dd4baaa17f4898913d7cf
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:22:06 +0000

    Delete createCharacterActor.sql

--- lokaverkefni/createTable/createCharacterActor.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CharacterActor(
-    id INTEGER PRIMARY KEY,
-    character_id INT NOT NULL,
-    staff_id INT NOT NULL,
-    FOREIGN KEY(character_id) REFERENCES Characters(id),
-    FOREIGN KEY(staff_id) REFERENCES Staff(id)
-);
-
-DROP TABLE CharacterActor;
