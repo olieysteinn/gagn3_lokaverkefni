a311bfb5fa1fd4bf525ed9cc51e473d5f8cb4434
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:27:07 +0000

    Delete createSoundtrackSongs.sql

--- GAGN3PS05_lokaverkefni/createTable/createSoundtrackSongs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE SoundtrackSongs(
-    id INTEGER PRIMARY KEY,
-    soundtrack_id INT NOT NULL,
-    song_id INT NOT NULL,
-    FOREIGN KEY(soundtrack_id) REFERENCES Soundtracks(id),
-    FOREIGN KEY(song_id) REFERENCES Songs(id)
-);
-
-DROP TABLE SoundtrackSongs;
