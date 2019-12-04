bcbdaa3f52a8fb6729f1ff27fb29ed6afc904727
Ólafur Eysteinn <olafurey@gmail.com>
2019-12-04 00:24:37 +0000

    Delete createStaff.sql

--- lokaverkefni/createTable/createStaff.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Staff(
-    id INTEGER PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
-    middle_name varchar(255),
-    last_name varchar(255) NOT NULL,
-    age DATE
-);
-
-DROP TABLE Staff;
